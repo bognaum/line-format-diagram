a92b350568d9ee3271878a5aa2a0aeaf4183fa38
Bognaum <bognaum@gmail.com>
2021-06-11 16:38:43 +0300

    4.0.2

--- lfd.js
+++ lfd.js
@@ -6,7 +6,7 @@ export default class LineFormatDiagram {
 		setStyle(clPref);
 	}
 
-	get version () {return "4.0.1"}
+	get version () {return "4.0.2"}
 
 	static get version () {return this.prototype.version;}
 
