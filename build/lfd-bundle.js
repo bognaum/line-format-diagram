0823ae0152a38851fc94f64a2a82a955af7d82a7
Bognaum <bognaum@gmail.com>
2021-06-28 09:37:06 +0300

    Fixed error of selection event.

--- build/lfd-bundle.js
+++ build/lfd-bundle.js
@@ -2023,15 +2023,24 @@ function setSelectionEvent(self) {
 		const 
 			sel = window.getSelection(),
 			rng = sel.rangeCount ? sel.getRangeAt(0) : null,
-			aC = rng?.commonAncestorContainer,
-			part = (function () {
-				let el = aC;
+			cAC = rng?.commonAncestorContainer,
+			rootPart = (function () {
+				let el = cAC;
 				do {
 					if (el.classList?.contains(`${self.clPref}-part`))
 						return el;
 				} while (el = el.parentElement);
-			})(),
-			serialN = part && parseInt(part.dataset.serialN),
+			})();
+		console.log(
+			"\n1. sel", sel, 
+			"\n2. region", rng, 
+			`\n3. cAC`, cAC, 
+			`\n4. rootPart`, rootPart, 
+		);
+		if (! rootPart)
+			return;
+		const
+			serialN = rootPart && parseInt(rootPart.dataset.serialN),
 			node = (function () {
 				let finded, sN = -1;
 				_lib_js__WEBPACK_IMPORTED_MODULE_0__.forEachRecur(node => {
@@ -2040,18 +2049,77 @@ function setSelectionEvent(self) {
 				}, self.editStage.tOb);
 				return finded;
 			})();
-		self.editStage.part = part;
+		self.editStage.rootPart = rootPart;
 		self.editStage.node = node;
 		self.editStage.range = rng;
 		console.log(
-			"\n1. sel", sel, 
-			"\n2. region", rng, 
-			`\n3. aC`, aC, 
-			`\n4. part`, part, 
 			"\n5. node", node,
 		);
 	});
 }
+/*
+function domEl(elA) {
+	return {
+		isAncestorOf: function(elB) {
+			return recur(elB);
+			function recur(el) {
+				if (elA == el) 
+					return true;
+				if (el.parentElement)
+					return recur(el.parentElement);
+			}
+		},
+		isDecendantOf: function(elB) {
+			return recur(elA);
+			function recur(el) {
+				if (elB == el) 
+					return true;
+				if (el.parentElement)
+					return recur(el.parentElement);
+			}
+		},
+		iheritsTo: function(elB) {
+			return recur(elA);
+			function recur(el) {
+				if (elB == el) 
+					return true;
+				if (el.parentElement)
+					return recur(el.parentElement);
+			}
+		},
+		enableClass: function(...args) {elA.classList.add(...args)},
+		disableClass: function(...args) {elA.classList.remove(...args)},
+		setClass: function(className, status=true) {
+			status ? elA.classList.add(className) : elA.classList.remove(className);
+		},
+		hasClass: function(...args) {return elA.classList.contains(...args)},
+	};
+}
+
+function getThisPart(el) {
+	return recur(el);
+	function recur(el) {
+		if (isPart(el)) 
+			return el;
+		if (el.parentElement)
+			return recur(el.parentElement);
+	}
+}
+
+function isPart(el) {
+	return el.classList.contains(`${self.clPref}-part`);
+}
+
+function getDomPath(el) {
+	const path = [];
+	recur(el);
+	return path;
+	function recur(el) {
+		path.unchift(el);
+		if (el.parentElement)
+			recur(el.parentElement);
+	}
+}*/
 
 /***/ }),
 /* 14 */
