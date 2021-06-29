a71ad912a257fbf8ca644a3c80b0258e9aeca3fb
Bognaum <bognaum@gmail.com>
2021-06-29 23:48:38 +0300

    Deleted unnecessary.

--- selection-event.js
+++ selection-event.js
@@ -4,39 +4,6 @@ export default function setSelectionEvent(self) {
 	// document.onselectionchange = function (ev) {
 	document.addEventListener("selectionchange", function (ev) {
 
-		const 
-			sel = window.getSelection(),
-			rng = sel.rangeCount ? sel.getRangeAt(0) : null,
-			cAC = rng?.commonAncestorContainer,
-			rootPart = (function () {
-				let el = cAC;
-				do {
-					if (el.classList?.contains(`${self.clPref}-part`))
-						return el;
-				} while (el = el.parentElement);
-			})();
-		console.log(
-			"\n1. sel", sel, 
-			"\n2. region", rng, 
-			`\n3. cAC`, cAC, 
-			`\n4. rootPart`, rootPart, 
-		);
-		if (! rootPart)
-			return;
-		/*console.log(`getDomPath(rng.startContainer)`, getDomPath(rng.startContainer));
-		console.log(`getDomPath(rng.endContainer)`, getDomPath(rng.endContainer));
-		console.log(`pathFrom(rootPart).to(rng.startContainer)`, pathFrom(rootPart).to(rng.startContainer));
-		console.log(`pathFrom(rootPart).to(rng.endContainer)`, pathFrom(rootPart).to(rng.endContainer));*/
-		const
-			serialN = rootPart && parseInt(rootPart.dataset.serialN),
-			rootNode = self.editStage.tOb.getBySerial(serialN);
-		self.editStage.rootPart = rootPart;
-		self.editStage.rootNode = rootNode;
-		self.editStage.range = rng;
-		console.log(
-			"\n5. rootNode", rootNode,
-		);
-
 		defineSelArgs(self);
 	});
 }
@@ -86,27 +53,6 @@ function defineSelArgs(self) {
 		b,
 	}
 	console.log(`self.editStage`, self.editStage);
-}
-
-function pathFrom(elA) {
-	return {
-		to: function(elB) {
-			const path = [];
-			if (recur(elB))
-				return path;
-			else 
-				return null;
-			function recur(el) {
-				path.unshift(el);
-				if (el == elA)
-					return true;
-				else if (el.parentElement)
-					return recur(el.parentElement);
-				else
-					return false;
-			}
-		}
-	};
 }
 
 function $elem(self) {
@@ -182,30 +128,5 @@ function $elem(self) {
 						return i;
 			},
 		};
-	}
-}
-
-function getThisPart(el) {
-	return recur(el);
-	function recur(el) {
-		if (isPart(el)) 
-			return el;
-		if (el.parentElement)
-			return recur(el.parentElement);
-	}
-}
-
-function isPart(el) {
-	return el.classList.contains(`${self.clPref}-part`);
-}
-
-function getDomPath(el) {
-	const path = [];
-	recur(el);
-	return path;
-	function recur(el) {
-		path.unshift(el);
-		if (el.parentElement)
-			recur(el.parentElement);
 	}
 }
